40d994af168c5b024549fe6385761c60673cf487
akshat-kaushik <93491696+akshat-kaushik@users.noreply.github.com>
2024-06-06 16:48:34 +0530

    first

---
+++ src/prisma/migrations/20240605182008_testingdbbb/migration.sql
@@ -0,0 +1,22 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `createdAt` on the `User` table. All the data in the column will be lost.
+  - You are about to drop the column `updatedAt` on the `User` table. All the data in the column will be lost.
+  - You are about to drop the `Doubt` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `Post` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `Project` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "User" DROP COLUMN "createdAt",
+DROP COLUMN "updatedAt";
+
+-- DropTable
+DROP TABLE "Doubt";
+
+-- DropTable
+DROP TABLE "Post";
+
+-- DropTable
+DROP TABLE "Project";
